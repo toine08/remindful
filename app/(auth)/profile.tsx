e948dfa1171657650ae35e2a22bc0267b43d2804
toine08 <riedotoine@gmail.com>
2024-01-14 17:28:48 +0100

    Add image upload functionality and loading indicator

--- app/(auth)/profile.tsx
+++ app/(auth)/profile.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity, Image, ActivityIndicator } from "react-native";
 import { useSupabase } from "@/hooks/useSupabase";
 import { supabase } from "@/config/supabase";
 import tw from "@/lib/tailwind";
@@ -7,7 +7,6 @@ import { Button } from "@/components/ui";
 import { registerForPushNotifications } from '../../lib/notifications';
 import { updatePushToken, getUsername } from "@/lib/utils";
 import * as ImagePicker from 'expo-image-picker';
-import { Image } from 'expo-image';
 import * as ImageManipulator from 'expo-image-manipulator';
 import * as FileSystem from 'expo-file-system';
 import { Buffer } from 'buffer';
@@ -17,10 +16,10 @@ import { Buffer } from 'buffer';
 
 export default function Profile() {
      const { signOut, user, session } = useSupabase();
-     const userSession = session?.access_token;
      const [username, setUsername] = useState<string | null>(null);
      const [tokenUpdated, setTokenUpdated] = useState(false);
-     const [image, setImage] = useState<string | null>(null); // Add this line
+     const [isLoading, setIsLoading] = useState(false);
+
      const pictureProfile = supabase.storage.from('avatars').getPublicUrl(`${user?.id}/avatar.png`) || "ya rien ";
 
      useEffect(() => {
@@ -36,6 +35,8 @@ export default function Profile() {
      }, [user, tokenUpdated]);
 
      async function uploadAvatar() {
+          setIsLoading(true);
+
 
           try {
 
@@ -87,22 +88,26 @@ export default function Profile() {
 
                const { error } = await supabase.storage.from('avatars').upload(`${user?.id}/avatar.png`, buffer, {
                     upsert: true,
-                    contentType: 'image/png',
+                    contentType: 'image/*',
                });
 
                if (error) {
                     console.error('Failed to upload image:', error);
+                    setIsLoading(false);
                     return;
                }
 
                console.log('Avatar uploaded successfully!');
-               setImage(`${supabase.storage.from('avatars').getPublicUrl(`${user?.id}/avatar.png`)}`);
+
 
           } catch (error) {
 
                console.error('Error uploading avatar:', error);
+               setIsLoading(false);
+
 
           }
+          setIsLoading(false);
 
      }
 
@@ -111,11 +116,23 @@ export default function Profile() {
                style={tw`flex-1 items-center justify-center bg-background pt-12 dark:bg-dark-background dark:text-white`}
           >
                <View>
-                    <Image
-                         source={`${pictureProfile.data.publicUrl}`}
-                         style={{ width: 100, height: 100, borderRadius: 50, backgroundColor: "red" }}
-                    />
-                    <Button label="Choisir une image" onPress={() => console.log(pictureProfile)} />
+                    <TouchableOpacity
+                         onPress={() => console.log("small press")}
+                         onLongPress={uploadAvatar}
+                         style={tw`flex-row items-center rounded-full h-25 w-25 justify-center ml-2 text-white bg-white/10`}
+                    >
+                         {isLoading ? (
+                              <ActivityIndicator size="large" color="#0000ff" />
+                         ) : (
+                              <Image
+                                   source={{ uri: pictureProfile.data.publicUrl }}
+                                   style={tw`h-25 w-25 rounded-full`}
+                              />
+                         )}
+
+                    </TouchableOpacity>
+
+
                     <Text style={tw`text-xl dark:text-white`}>Hello {username}</Text>
                     <Button label="SignOut" textStyle={tw`font-bold`} onPress={signOut} />
                </View>
